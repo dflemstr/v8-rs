99f9ceebbabc4988f4ecb8e18edb7d4e0e8d4532
David Flemström <david.flemstrom@gmail.com>
2016-09-25 23:53:46 -0400

    Grant access to global object

--- src/context.rs
+++ src/context.rs
@@ -1,13 +1,25 @@
 use v8_sys as v8;
 use isolate;
 use util;
+use value;
 
 #[derive(Debug)]
 pub struct Context<'a>(&'a isolate::Isolate, v8::ContextRef);
 
 impl<'a> Context<'a> {
     pub fn new(isolate: &isolate::Isolate) -> Context {
-        unsafe { Context(isolate, util::invoke(isolate, |c| v8::Context_New(c)).unwrap()) }
+        unsafe {
+            Context(isolate,
+                    util::invoke(isolate, |c| v8::Context_New(c)).unwrap())
+        }
+    }
+
+    pub fn global(&self) -> value::Object<'a> {
+        unsafe {
+            value::Object::from_raw(self.0,
+                                    util::invoke(self.0, |c| v8::Context_Global(c, self.1))
+                                        .unwrap())
+        }
     }
 
     pub unsafe fn from_raw(isolate: &isolate::Isolate, raw: v8::ContextRef) -> Context {
